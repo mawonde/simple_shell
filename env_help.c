6518f204f04768d75ffe55d60c484fe1e57dd5b0
manqobadube <manqoba.dube7@gmail.com>
2023-04-24 23:36:42 -0700

    simple shell project for team work

---
+++ env_help.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+void env_help(void);
+void setenv_help(void);
+void unsetenv_help(void);
+void history_help(void);
+
+/**
+ * env_help - Displays information on the simple_shell builtin command 'env'.
+ */
+void env_help(void)
+{
+	char *msg = "env: env\n\tPrints the current environment.\n";
+
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+}
+
+/**
+ * setenv_help - Displays information on the simple_shell builtin command 'setenv'.
+ */
+void setenv_help(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+}
+
+/**
+ * unsetenv_help - Displays information on the simple_shell builtin command
+ * 'unsetenv'.
+ */
+void unsetenv_help(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, custom_strlen(msg));
+}
+
